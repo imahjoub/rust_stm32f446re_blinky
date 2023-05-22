3d86446eaa5b39ae8706a1af85c647e5c78a2393
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-05-22 12:29:37 +0200

    keep working on fmt

--- examples/led_blinky.rs
+++ examples/led_blinky.rs
@@ -2,60 +2,36 @@
 #![no_main]
 #![no_std]
 
-// Halt on panic
-use panic_halt as _; // panic handler
+#[allow(unused)]
+use panic_halt;
 
-use cortex_m;
-use cortex_m_rt::entry;
+use cortex_m_rt as rt;
 use stm32f4xx_hal as hal;
+use hal::prelude::*; // needed for the GpioExt trait (-> .split)
 
-use crate::hal::{prelude::*, serial::config::Config, serial::Serial, stm32};
 
-use core::fmt::Write;
+#[rt::entry]
+fn main() -> !
+{
+  if let Some(peripherals) = hal::stm32::Peripherals::take()
+  {
+    // + sets RCC->AHB1ENR GPIOA bit
+    let gpioa = peripherals.GPIOA.split();
 
-#[entry]
-fn main() -> ! {
-    if let (Some(dp), Some(cp)) = (
-        stm32::Peripherals::take(),
-        cortex_m::peripheral::Peripherals::take(),
-    ) {
-        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
-        let gpioa = dp.GPIOA.split();
-        let mut led = gpioa.pa5.into_push_pull_output();
+    // .into_push_pull_output performs three steps
+    // 1) set PUPDR: 00 -> no pull-up, no pull-down
+    // 2) set OTYPER: 0 -> output push-pull
+    // 3) set MODER: 01 -> general purpose output mode
+    let mut led = gpioa.pa5.into_push_pull_output();
 
-        let tx_pin = gpioa.pa2.into_alternate_af7();
-        let rx_pin = gpioa.pa3.into_alternate_af7();
-
-        // Set up the system clock. We want to run at 84MHz for this one.
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(84.mhz()).freeze();
-
-        let serial = Serial::usart2(
-            dp.USART2,
-            (tx_pin, rx_pin),
-            Config::default().baudrate(115_200.bps()),
-            clocks,
-        )
-        .unwrap();
-
-        let (mut tx, mut _rx) = serial.split();
-
-        // Create a delay abstraction based on SysTick
-        let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
-
-        loop {
-            // On for 1s, off for 1s.
-            if led.set_high().is_err() {
-                // Handle error?
-            }
-            delay.delay_ms(1000_u32);
-            if led.set_low().is_err() {
-                // Handle error?
-            }
-            delay.delay_ms(1000_u32);
-            writeln!(tx, "Hello, world!\r",).unwrap();
-        }
+    loop
+    {
+      led.set_low().unwrap();
+      for _ in 0..50_000 {}
+      led.set_high().unwrap();
+      for _ in 0..50_000 {}
     }
+  }
 
-    loop {}
+  loop {}
 }
