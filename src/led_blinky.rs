f8a4a11e205ec8913d0374f03d25e9338fa32ca2
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-05-22 17:57:49 +0200

    update led_blinky.rs

--- src/led_blinky.rs
+++ src/led_blinky.rs
@@ -1,4 +1,3 @@
-#![deny(unsafe_code)]
 // std and main are not available for bare metal software
 #![no_main]
 #![no_std]
@@ -6,6 +5,7 @@
 use cortex_m_rt::entry;       // The runtime
 use stm32f4xx_hal as hal;
 use hal::prelude::*;          // needed for the GpioExt trait (-> .split)
+
 #[allow(unused)]
 use panic_halt;               // When a panic occurs, stop the microcontroller
 
@@ -13,27 +13,23 @@ use panic_halt;               // When a panic occurs, stop the microcontroller
 // startup code before this, but we don't need to worry about this
 #[entry]
 fn main() -> ! {
-    if let Some(peripherals) = hal::stm32::Peripherals::take() {
-        let gpioa = peripherals.GPIOA.split(); // + sets RCC->AHB1ENR GPIOA bit
+  if let Some(peripherals) = hal::stm32::Peripherals::take() {
+      // Set RCC->AHB1ENR GPIOA bit
+      let gpioa = peripherals.GPIOA.split();
 
-        // .into_push_pull_output performs three steps
-        // 1) set PUPDR: 00 -> no pull-up, no pull-down
-        // 2) set OTYPER: 0 -> output push-pull
-        // 3) set MODER: 01 -> general purpose output mode
-        let mut led = gpioa.pa5.into_push_pull_output();
+      // .into_push_pull_output performs three steps
+      // 1) set PUPDR: 00 -> no pull-up, no pull-down
+      // 2) set OTYPER: 0 -> output push-pull
+      // 3) set MODER: 01 -> general purpose output mode
+      let mut led = gpioa.pa5.into_push_pull_output();
 
-        let gpioc = peripherals.GPIOC.split();
-        let button = gpioc.pc13; // pins are input by default
-
-        loop {
-            // .is_high reads IDR
-            if button.is_high().unwrap() {
-                // .set_low uses BSRR
-                led.set_low().unwrap();
-            } else {
-                led.set_high().unwrap();
-            }
-        }
-    }
-    loop {}
+      loop {
+          // .toggle pin
+              led.set_low().unwrap();
+              for _ in 0..50_000 {}
+              led.set_high().unwrap();
+              for _ in 0..50_000 {}
+      }
+  }
+  loop {}
 }
