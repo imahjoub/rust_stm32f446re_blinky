babbcfa1b351fc107a5e52b518c78dfeb9b2cbbe
Gabriel Dobato <dobatog@gmail.com>
2023-06-02 20:37:40 +0200

    Fix format and format check in CI

--- src/led_blinky.rs
+++ src/led_blinky.rs
@@ -2,38 +2,35 @@
 #![no_main]
 #![no_std]
 
-use cortex_m_rt::entry;       // The runtime
-use stm32f4xx_hal as hal;
-use hal::prelude::*;          // needed for the GpioExt trait (-> .split)
+use cortex_m_rt::entry; // The runtime
+use hal::prelude::*;
+use stm32f4xx_hal as hal; // needed for the GpioExt trait (-> .split)
 
 #[allow(unused)]
-use panic_halt;               // When a panic occurs, stop the microcontroller
+use panic_halt; // When a panic occurs, stop the microcontroller
 
 // This marks the entrypoint of our application. The cortex_m_rt creates some
 // startup code before this, but we don't need to worry about this
 #[entry]
-fn main() -> !
-{
-  if let Some(peripherals) = hal::stm32::Peripherals::take()
-  {
-    // Set RCC->AHB1ENR GPIOA bit
-    let gpioa = peripherals.GPIOA.split();
+fn main() -> ! {
+    if let Some(peripherals) = hal::stm32::Peripherals::take() {
+        // Set RCC->AHB1ENR GPIOA bit
+        let gpioa = peripherals.GPIOA.split();
 
-    // .into_push_pull_output performs three steps
-    // 1) set PUPDR: 00 -> no pull-up, no pull-down
-    // 2) set OTYPER: 0 -> output push-pull
-    // 3) set MODER: 01 -> general purpose output mode
-    let mut led = gpioa.pa5.into_push_pull_output();
+        // .into_push_pull_output performs three steps
+        // 1) set PUPDR: 00 -> no pull-up, no pull-down
+        // 2) set OTYPER: 0 -> output push-pull
+        // 3) set MODER: 01 -> general purpose output mode
+        let mut led = gpioa.pa5.into_push_pull_output();
 
-    loop
-    {
-      // toggle pin
-      led.set_low().unwrap();
-      for _ in 0..50_000 {}
-      led.set_high().unwrap();
-      for _ in 0..50_000 {}
+        loop {
+            // toggle pin
+            led.set_low().unwrap();
+            for _ in 0..50_000 {}
+            led.set_high().unwrap();
+            for _ in 0..50_000 {}
+        }
     }
-  }
 
-  loop {}
+    loop {}
 }
